d7b788314d81e69e895ff18d437874080457589f
Hyoung Il Jin <gjfaos1245@naver.com>
2018-11-14 20:34:42 +0900

    PP10.Polymorphism

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -17,9 +17,17 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 		{
 			return false;
 		}
+		m_go = new GameObject();
+		m_player = new Player();
+		m_enemy = new Enemy();
 
-		m_go.load(100, 100, 128, 82, "animate");
-		m_player.load(300, 300, 128, 82, "animate");
+		m_go->load(100, 100, 128, 82, "animate");
+		m_player->load(300, 300, 128, 82, "animate");
+		m_enemy->load(0, 0, 128, 82, "animate");
+
+		m_gameObjects.push_back(m_go);
+		m_gameObjects.push_back(m_player);
+		m_gameObjects.push_back(m_enemy);
 
 		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
 
@@ -41,8 +49,11 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
-	m_go.draw(m_pRenderer);
-	m_player.draw(m_pRenderer);
+	for (std::vector<GameObject*>::size_type i = 0;
+		i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->draw(m_pRenderer);
+	}
 	SDL_RenderPresent(m_pRenderer);
 }
 
@@ -72,6 +83,9 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	m_go.update();
-	m_player.update();
+	for (std::vector<GameObject*>::size_type i = 0;
+		i != m_gameObjects.size(); i++)
+	{
+		m_gameObjects[i]->update();
+	}
 }
--- GameFramework/PP01.HelloSDL/Game.h
+++ GameFramework/PP01.HelloSDL/Game.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "TextureManager.h"
 #include "Player.h"
+#include "Enemy.h"
+#include <vector>
 
 class Game
 {
+	std::vector<GameObject*> m_gameObjects;
 public:
 	Game() {}
 	~Game() {}
@@ -15,8 +18,9 @@ public:
 	bool running() { return m_bRunning; }
 	int m_currentFrame;
 	TextureManager m_textureManager;
-	GameObject m_go;
-	Player m_player;
+	GameObject* m_go;
+	GameObject* m_player;
+	GameObject* m_enemy;
 
 private:
 	SDL_Window * m_pWindow;
